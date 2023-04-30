e042d7bb4aedcba2acd5487937cd3c62b5416558
sheryjebu <sheryjebu@gmail.com>
2023-04-30 20:14:39 +0100

    test commit

---
+++ ECommerce.Store.WebApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Store.WebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. we are adding DB context to our services or dependency injection container
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. addind the middleware we should have a order
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
