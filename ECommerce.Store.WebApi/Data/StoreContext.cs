edc7a135746fdfb5db7499dc1cd3a15025342b43
sheryjebu <sheryjebu@gmail.com>
2023-04-30 20:22:03 +0100

    test commit

---
+++ ECommerce.Store.WebApi/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ECommerce.Store.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Store.WebApi.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }// resembles product table
+    }
+}
+
